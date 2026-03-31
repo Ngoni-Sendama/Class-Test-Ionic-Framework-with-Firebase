96742260f2c9c5b931713544c33c9185be624f60
Ngoni-Sendama <ngonidzashesendama@gmail.com>
2026-03-31 11:40:35 +0530

    UI Design

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonBadge, IonButton } from '@ionic/angular/standalone';
+
+import { addIcons } from 'ionicons';
+import {  notificationsCircleOutline, notificationsOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonIcon, IonHeader, IonToolbar, IonTitle, IonContent, IonBadge, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ notificationsOutline });
+  }
 }
