eea679e51d098bdb20752102ec7a1a094ecd63ad
Ngoni-Sendama <ngonidzashesendama@gmail.com>
2026-03-31 15:29:27 +0530

    Updates on the app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,6 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyBsEM9rqeo291r7d1Yyijkjh1su9i8W6Yo",
-    authDomain: "ca3test-7fcd3.firebaseapp.com",
-    projectId: "ca3test-7fcd3",
-    storageBucket: "ca3test-7fcd3.firebasestorage.app",
-    messagingSenderId: "209330059195",
-    appId: "1:209330059195:web:bdaa03a344791d8461369e",
-    measurementId: "G-NVMQB8BF5G"
+   
   }
 };
